44609e223ef9d885840533083202f3d8edb22226
Daaiki <daaikii@outlook.jp>
2021-06-06 16:32:56 +0900

    add seo

--- src/pages/En.tsx
+++ src/pages/En.tsx
@@ -1,7 +1,14 @@
+import { Helmet } from 'react-helmet'
 import { SearchDate } from '../components/SearchDate'
 
 export const En = () => {
   return (
-    <SearchDate Yes="Yes" No="No" />
+    <div>
+      <Helmet>
+        <title>Is Wakayama University founded?</title>
+        <meta name="description" content="This is the site where you can check if today is the anniversary of Wakayama University's founding." />
+      </Helmet>
+      <SearchDate Yes="Yes" No="No" />
+    </div>
   )
 } 
--- src/pages/Ja.tsx
+++ src/pages/Ja.tsx
@@ -1,7 +1,14 @@
+import { Helmet } from 'react-helmet'
 import { SearchDate } from '../components/SearchDate'
 
 export const Ja = () => {
   return (
-    <SearchDate Yes="はい" No="いいえ" />
+    <div>
+      <Helmet>
+        <title>和歌山大学の創立記念日ですか？</title>
+        <meta name="description" content="今日が和歌山大学の創立記念日かどうか確認できるサイトです。" />
+      </Helmet>
+      <SearchDate Yes="はい" No="いいえ" />
+    </div>
   )
 }
