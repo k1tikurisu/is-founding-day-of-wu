2b44a4bde4fda595fb2018bcf3c796753580719b
Daaiki <daaikii@outlook.jp>
2021-06-06 15:01:04 +0900

    setup webpack with react, babel and typescript

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  entry: path.join(__dirname, 'src', 'main.tsx'),
+  output: {
+    path: path.join(__dirname, 'docs'),
+    filename: 'main.bundle.js',
+  },
+  mode: process.env.NODE_ENV || 'development',
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'src')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['ts-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html')
+    })
+  ]
+}
