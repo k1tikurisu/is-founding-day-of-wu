e2263a745cdca877fedb2feb31b7b6435f5d23ef
Daaiki <daaikii@outlook.jp>
2021-06-07 21:57:40 +0900

    add Google Fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import WebFont from 'webfontloader'
+import './main.css'
 
 import { App } from './App'
+
+WebFont.load({
+  google: {
+    families: ['Noto Sans JP:400,700', 'sans-serif'],
+  },
+})
 
 ReactDOM.render(<App />, document.getElementById('root'))
